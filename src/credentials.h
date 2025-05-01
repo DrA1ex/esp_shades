6248e7f84f2cf366efe090fad97582a3a56a752f
Alexander <DrA1ex@users.noreply.github.com>
2025-05-01 13:50:04 +0500

    Initial commit

---
+++ src/credentials.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define WIFI_SSID                                    "ESP_LED"
+#define WIFI_PASSWORD                                "12345678"
+
+#define WEB_AUTH_USER                                "esp_led"
+#define WEB_AUTH_PASSWORD                            "password"
+
+#define MQTT_HOST                                    "example.com"
+#define MQTT_PORT                                    (1234u)
+#define MQTT_USER                                    "esp_user"
+#define MQTT_PASSWORD                                "esp_pass"
